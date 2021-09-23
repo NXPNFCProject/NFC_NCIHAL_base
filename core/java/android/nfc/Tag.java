6860afeb1dd1fc452fa3af99921197cc7aa1aad2
nxf56319 <sai.shwethas@nxp.com>
2021-09-23 12:54:34 +0530

    QSSI Alignment
    Support for Q-TAG

--- core/java/android/nfc/Tag.java
+++ core/java/android/nfc/Tag.java
@@ -13,7 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/******************************************************************************
+*
+*  The original Work has been changed by NXP.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Copyright 2021 NXP
+*
+******************************************************************************/
 package android.nfc;
 
 import android.compat.annotation.UnsupportedAppUsage;
@@ -191,6 +209,9 @@ public final class Tag implements Parcelable {
                     break;
                 case TagTechnology.NFC_BARCODE:
                     strings[i] = NfcBarcode.class.getName();
+                    break;
+                case TagTechnology.NFC_Q:
+                    strings[i] = "NfcQ";
                     break;
                 default:
                     throw new IllegalArgumentException("Unknown tech type " + techList[i]);
--- core/java/android/nfc/tech/TagTechnology.java
+++ core/java/android/nfc/tech/TagTechnology.java
@@ -13,7 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/******************************************************************************
+*
+*  The original Work has been changed by NXP.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Copyright 2021 NXP
+*
+******************************************************************************/
 package android.nfc.tech;
 
 import android.nfc.Tag;
@@ -156,6 +174,13 @@ public interface TagTechnology extends Closeable {
      * @hide
      */
     public static final int NFC_BARCODE = 10;
+
+    /**
+     * This technology is an instance of {@link NfcQ}.
+     * <p>Support for this technology type is mandatory.
+     * @hide
+     */
+    public static final int NFC_Q = 11;
 
     /**
      * Get the {@link Tag} object backing this {@link TagTechnology} object.
