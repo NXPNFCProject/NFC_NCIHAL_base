590099b2a1454ee3c81aa3f1854851dc1fb10e2b
nxf32288 <ganesh.deva_1@nxp.com>
2022-07-14 15:42:13 +0530

    Add tag availability checking mechanism
    Set a cookie to the tag objects when they are constructed, the
    cookie could be used to check the availability before returning
    the tag service

--- core/java/android/nfc/Tag.java
+++ core/java/android/nfc/Tag.java
@@ -52,6 +52,7 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.RemoteException;
+import android.os.SystemClock;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -139,6 +140,7 @@ public final class Tag implements Parcelable {
     final INfcTag mTagService; // interface to NFC service, will be null if mock tag
 
     int mConnectedTechnology;
+    long mCookie;
 
     /**
      * Hidden constructor to be used by NFC service and internal classes.
@@ -158,6 +160,13 @@ public final class Tag implements Parcelable {
         mTagService = tagService;
 
         mConnectedTechnology = -1;
+
+        try {
+            mCookie = SystemClock.elapsedRealtime();
+            tagService.setTagUpToDate(mCookie);
+        } catch (RemoteException e) {
+            throw e.rethrowAsRuntimeException();
+        }
     }
 
     /**
@@ -382,6 +391,18 @@ public final class Tag implements Parcelable {
     /** @hide */
     @UnsupportedAppUsage
     public INfcTag getTagService() {
+        try {
+            if (!mTagService.isTagUpToDate(mCookie)) {
+                String id_str = "";
+                for (int i = 0; i < mId.length; i++) {
+                    id_str = id_str + String.format("%02X ", mId[i]);
+                }
+                String msg = "Permission Denial: Tag ( ID: " + id_str + ") is out of date";
+                throw new SecurityException(msg);
+            }
+        } catch (RemoteException e) {
+            throw e.rethrowAsRuntimeException();
+        }
         return mTagService;
     }
 
