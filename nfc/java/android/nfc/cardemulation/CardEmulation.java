fa5b62d8eb11339806b6bb6e16afa896afad3694
nxf71239 <krnaveen.kumar@nxp.com>
2024-11-30 17:19:23 +0530

    nfc: Remove usage of "eSIM" from offhost names
    Partial revert of aosp/3253198
    
    Bug: 321314635
    
    Google URL: https://android-review.googlesource.com/c/platform/frameworks/base/+/3273424

--- nfc/java/android/nfc/cardemulation/CardEmulation.java
+++ nfc/java/android/nfc/cardemulation/CardEmulation.java
@@ -568,13 +568,11 @@ public final class CardEmulation {
 
         List<String> validSE = adapter.getSupportedOffHostSecureElements();
         if ((offHostSecureElement.startsWith("eSE") && !validSE.contains("eSE"))
-                || (offHostSecureElement.startsWith("SIM") && !validSE.contains("SIM"))
-                || (offHostSecureElement.startsWith("eSIM") && !validSE.contains("eSIM"))) {
+                || (offHostSecureElement.startsWith("SIM") && !validSE.contains("SIM"))) {
             return false;
         }
 
-        if (!offHostSecureElement.startsWith("eSE") && !offHostSecureElement.startsWith("SIM")
-                && !(Flags.enableCardEmulationEuicc() && offHostSecureElement.startsWith("eSIM"))) {
+        if (!offHostSecureElement.startsWith("eSE") && !offHostSecureElement.startsWith("SIM")) {
             return false;
         }
 
@@ -582,8 +580,6 @@ public final class CardEmulation {
             offHostSecureElement = "eSE1";
         } else if (offHostSecureElement.equals("SIM")) {
             offHostSecureElement = "SIM1";
-        } else if (Flags.enableCardEmulationEuicc() && offHostSecureElement.equals("eSIM")) {
-            offHostSecureElement = "eSIM1";
         }
 
         final String offHostSecureElementV = new String(offHostSecureElement);
