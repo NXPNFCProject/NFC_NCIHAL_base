6f7cab0b4d1909b7bd760df6ff32e50338f40946
nxf71239 <krnaveen.kumar@nxp.com>
2024-12-02 18:09:01 +0530

    Extending setControllerAlwaysOn feature with trasnparent and Card Emulation mode support
    Bug: 331563246
    nfcOemExtension.setControllerAlwaysOn(INIT_MODE_TRANSPARENT)
    nfcOemExtension.setControllerAlwaysOn(INIT_MODE_EE)
    
    Google URL: https://android-review.googlesource.com/c/platform/frameworks/base/+/3148216
    
    Change-Id: I5062927b9b45c51297afc5bc7c09eabbfc01058c

--- nfc/java/android/nfc/NfcAdapter.java
+++ nfc/java/android/nfc/NfcAdapter.java
@@ -577,6 +577,18 @@ public final class NfcAdapter {
     @Retention(RetentionPolicy.SOURCE)
     public @interface TagIntentAppPreferenceResult {}
 
+    /**
+     * Mode Type for {@link NfcOemExtension#setControllerAlwaysOn(int)}.
+     * @hide
+     */
+    public static final int CONTROLLER_ALWAYS_ON_MODE_DEFAULT = 1;
+
+    /**
+     * Mode Type for {@link NfcOemExtension#setControllerAlwaysOn(int)}.
+     * @hide
+     */
+    public static final int CONTROLLER_ALWAYS_ON_DISABLE = 0;
+
     // Guarded by sLock
     static boolean sIsInitialized = false;
     static boolean sHasNfcFeature;
@@ -2343,7 +2355,8 @@ public final class NfcAdapter {
      * FEATURE_NFC_HOST_CARD_EMULATION, FEATURE_NFC_HOST_CARD_EMULATION_NFCF,
      * FEATURE_NFC_OFF_HOST_CARD_EMULATION_UICC and FEATURE_NFC_OFF_HOST_CARD_EMULATION_ESE
      * are unavailable
-     * @return void
+     * @return true if feature is supported by the device and operation has bee initiated,
+     * false if the feature is not supported by the device.
      * @hide
      */
     @SystemApi
@@ -2352,8 +2365,13 @@ public final class NfcAdapter {
         if (!sHasNfcFeature && !sHasCeFeature) {
             throw new UnsupportedOperationException();
         }
-        return callServiceReturn(() ->  sService.setControllerAlwaysOn(value), false);
-
+        int mode = value ? CONTROLLER_ALWAYS_ON_MODE_DEFAULT : CONTROLLER_ALWAYS_ON_DISABLE;
+        try {
+            callService(() -> sService.setControllerAlwaysOn(mode));
+        } catch (UnsupportedOperationException e) {
+            return false;
+        }
+        return true;
     }
 
     /**
